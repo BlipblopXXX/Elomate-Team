ff979a14ba07c15addb1cd34b55d95905f700582
Dwiko Adani <122418604+dwikoadnn@users.noreply.github.com>
2024-06-07 09:32:25 +0700

    Update Profile.js

--- src/Mentee/Profile.js
+++ src/Mentee/Profile.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import './Profile.css';
-import { MonthView } from 'react-calendar';
 
 // const AdditionalActivities = [
 //     {name: 'Social activity1', title: '1'},
@@ -14,17 +13,88 @@ import { MonthView } from 'react-calendar';
 //     {name: 'Social activity3', title: '3'},
 // ]
 
-const Additional = [
-    {
-      activities: [
-        { activity: 'Social Activity1', Date: '15 juli 2024', role: 'Peran', description: 'Deskripsi', },
-        { activity: 'Social Activity2', Date: '15 juli 2024', role: 'Peran', description: 'Deskripsi', },
-        { activity: 'Social Activity3', Date: '15 juli 2024', role: 'Peran', description: 'Deskripsi', },
-      ]
-    
-    }
-]
 function Profile() {
+
+    const Additional = [
+        {
+          activities: [
+            { activity: 'Social Activity1', Date: '15 juli 2024', role: 'Peran', description: 'Deskripsi', },
+            { activity: 'Social Activity2', Date: '15 juli 2024', role: 'Peran', description: 'Deskripsi', },
+            { activity: 'Social Activity3', Date: '15 juli 2024', role: 'Peran', description: 'Deskripsi', },
+          ]
+        
+        }
+    ]
+    
+    const [fullName, setFullName] = useState('');
+    const handleInputName = (event) => {
+        setFullName(event.target.value);
+    };
+
+    const [NRP, setNRP] = useState('890837829');
+    const handleInputNRP = (event) => {
+        setNRP(event.target.value);
+    };
+
+    const [posisi, setPosisi] = useState('');
+    const handleInputPosisi = (event) => {
+        setPosisi(event.target.value);
+    };
+
+    const [tempatLahir, setTempatLahir] = useState('');
+    const handleInputTempat = (event) => {
+        setTempatLahir(event.target.value);
+    };
+
+    const [date, setDate] = useState(new Date());
+    const formatDate = (date) => {
+        let d = new Date(date),
+          day = '' + d.getDate(),
+            month = '' + (d.getMonth() + 1),
+            year = d.getFullYear();
+    
+        if (month.length < 2) 
+            month = '0' + month;
+        if (day.length < 2) 
+            day = '0' + day;
+    
+        return [year, month, day].join('-');
+      }
+
+    const [universitas, setUniversitas] = useState('');
+    const handleInputUniversitas = (event) => {
+        setUniversitas(event.target.value);
+    };
+
+    const [domisili, setDomisili] = useState('');
+    const handleInputDomisili = (event) => {
+        setDomisili(event.target.value);
+    };
+
+    const [nomor, setNomor] = useState('');
+    const handleInputNomor = (event) => {
+        setNomor(event.target.value);
+    };
+
+    const [email, setEmail] = useState('');
+    const handleInputEmail = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handleSaveButton = (event) => {
+        handleMain();
+    }
+
+    const [password, setPassword] = useState('');
+    const handleInputPassword = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const handleInputConfirmPassword = (event) => {
+        setConfirmPassword(event.target.value);
+    };
+
     const [currentPage, setCurrentPage] = useState('main');
     const handleMain = () => {
         setCurrentPage('main');
@@ -42,8 +112,12 @@ function Profile() {
         setCurrentPage('five');
     }
 
-    const handleEditAccount = () => {
+    const handleEditProfile = () => {
         setCurrentPage('second');
+    }
+
+    const handleEditAccount = () => {
+        setCurrentPage('three');
     }
 
     
@@ -79,7 +153,7 @@ function Profile() {
                                     <hr />
                                     <div className="input-Email"><b>Email :</b></div>
                                 </div>
-                                <div className='EditProfile' onClick={handleEditAccount}> Edit Profile </div>
+                                <div className='EditProfile' onClick={handleEditProfile}> Edit Profile </div>
                             </div>
                             <hr />
                             <div className='bot-container'>
@@ -96,19 +170,19 @@ function Profile() {
                                                             <div className="activity">{AdditionalActivities.activity}</div>
                                                         </div>
                                                         <div className='activity-row'>
-                                                            <img src='/src/files/icons/dateadditional.png'/><div className="24 Juli 2024">{AdditionalActivities.Date}</div>
+                                                            <img className='icon' src='/src/files/icons/dateadditional.png'/><div className="24 Juli 2024">{AdditionalActivities.Date}</div>
                                                         </div>
                                                         <div className='activity-row'>
-                                                            <img src='/src/files/icons/Roleadditional.png'/><div className="Peran">{AdditionalActivities.role}</div>
+                                                            <img className='icon' src='/src/files/icons/Roleadditional.png'/><div className="Peran">{AdditionalActivities.role}</div>
                                                         </div>
                                                         <div className='activity-row'>
-                                                            <img src='/src/files/icons/desc.png'/><div className="Deskripsi">{AdditionalActivities.description} </div>
+                                                            <img className='icon' src='/src/files/icons/desc.png'/><div className="Deskripsi">{AdditionalActivities.description} </div>
                                                         </div>
                                                     </div>
-                                                    <div>
-                                                        <img className='viewButton' src='/src/files/icons/buttonview.png'/>
-                                                        <img className='deletbutton'src='/src/files/icons/deletebutton.png'/>
+                                                    <div className='buttons'>
                                                         <img className='editbutton'src='/src/files/icons/editbutton.png'/>
+                                                        <img className='deletebutton'src='/src/files/icons/deletebutton.png'/>
+                                                        <img className='viewButton' src='/src/files/icons/buttonview.png'/>
                                                     </div>
                                                 </div>
                                                 ))}
@@ -124,12 +198,191 @@ function Profile() {
             case 'second':
                 return(
                     <div className='profile'>
-                        <div className='editprofile-container'>test</div>
+                        <div className="title2">
+                            <img className="backbutton" onClick={handleMain} src="/src/files/icons/backbutton.png" />
+                            <h><b>Edit Profile</b></h>
+                        </div>
+                        <hr />
+
+                        <div className='editprofile-container'>
+                            <div className='picture-container'>
+                                <img className="pictureEdit" src="/src/files/profile/Profile1.png" />
+                            </div>
+                            <label>
+                                <div>
+                                    <p className='judul'>Nama Lengkap</p>
+                                    <label className='inputText'>
+                                        <input
+                                        type="text"
+                                        className='inputan'
+                                        value={fullName}
+                                        onChange={handleInputName}
+                                        />
+                                    </label>
+                                </div>
+                            </label>
+                            <label>
+                                <div>
+                                    <p className='judul'>NRP</p>
+                                    <label className='inputText'>
+                                        <input
+                                        type="text"
+                                        disabled
+                                        className='inputan'
+                                        value={NRP}
+                                        onChange={handleInputNRP}
+                                        />
+                                    </label>
+                                </div>
+                            </label>
+                            <label>
+                                <div>
+                                    <p className='judul'>Posisi</p>
+                                    <label className='inputText'>
+                                        <input
+                                        type="text"
+                                        className='inputan'
+                                        value={posisi}
+                                        onChange={handleInputPosisi}
+                                        />
+                                    </label>
+                                </div>
+                            </label>
+                            <label>
+                                <div>
+                                    <p className='judul'>Tempat Lahir</p>
+                                    <label className='inputText'>
+                                        <input
+                                        type="text"
+                                        className='inputan'
+                                        value={tempatLahir}
+                                        onChange={handleInputTempat}
+                                        />
+                                    </label>
+                                </div>
+                            </label>
+                            <p className='judul'>Tanggal Lahir</p>
+                            <input
+                                type="date"
+                                className="dateInput"
+                                value={formatDate(date)}
+                                onChange={(e) => setDate(new Date(e.target.value))}
+                            />
+                            <label>
+                                <div>
+                                    <p className='judul'>Asal Universitas</p>
+                                    <label className='inputText'>
+                                        <input
+                                        type="text"
+                                        className='inputan'
+                                        value={universitas}
+                                        onChange={handleInputUniversitas}
+                                        />
+                                    </label>
+                                </div>
+                            </label>
+                            <label>
+                                <div>
+                                    <p className='judul'>Domisili</p>
+                                    <label className='inputText'>
+                                        <input
+                                        type="text"
+                                        className='inputan'
+                                        value={domisili}
+                                        onChange={handleInputDomisili}
+                                        />
+                                    </label>
+                                </div>
+                            </label>
+                            <label>
+                                <div>
+                                    <p className='judul'>No. HP</p>
+                                    <label className='inputText'>
+                                        <input
+                                        type="Phone number"
+                                        className='inputan'
+                                        value={nomor}
+                                        onChange={handleInputNomor}
+                                        />
+                                    </label>
+                                </div>
+                            </label>
+                            <label>
+                                <div>
+                                    <p className='judul'>Email</p>
+                                    <label className='inputText'>
+                                        <input
+                                        type="email"
+                                        className='inputan'
+                                        value={email}
+                                        onChange={handleInputEmail}
+                                        />
+                                    </label>
+                                </div>
+                            </label>
+                            <button className='saveButton' onClick={handleSaveButton}>
+                                Save
+                            </button>
+                        </div>
                     </div>
                 )
                 
             case 'three':
-                return null;
+                return (
+                    <div className='profile'>
+                        <div className="title2">
+                            <img className="backbutton" onClick={handleMain} src="/src/files/icons/backbutton.png" />
+                            <h><b>Edit Account</b></h>
+                        </div>
+                        <hr />
+                    <div className='editaccount-container'>
+                        <label>
+                            <div>
+                                <p className='judul'>NRP</p>
+                                <label className='inputText'>
+                                    <input
+                                    type="text"
+                                    disabled
+                                    className='inputan'
+                                    value={NRP}
+                                    onChange={handleInputNRP}
+                                    />
+                                </label>
+                            </div>
+                        </label>
+                        <label>
+                            <div>
+                                <p className='judul'>Password</p>
+                                <label className='inputText'>
+                                    <input
+                                    type="password"
+                                    className='inputan'
+                                    value={password}
+                                    onChange={handleInputPassword}
+                                    />
+                                </label>
+                            </div>
+                        </label>
+                        <label>
+                            <div>
+                                <p className='judul'>Confirm Password</p>
+                                <label className='inputText'>
+                                    <input
+                                    type="password"
+                                    className='inputan'
+                                    value={confirmPassword}
+                                    onChange={handleInputConfirmPassword}
+                                    />
+                                </label>
+                            </div>
+                        </label>
+                        <button className='saveButton' onClick={handleSaveButton}>
+                            Save
+                        </button>
+                    </div>
+
+                    </div>
+                )
             case 'four':
                 return null;
             case 'five':
