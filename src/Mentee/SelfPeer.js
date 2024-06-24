c797f297081f284aebad9b1e7f2b388da18dd39a
Inori Muira Sitanggang <118002715+121140020INORI@users.noreply.github.com>
2024-06-24 10:14:19 +0700

    Update SelfPeer.js

--- src/Mentee/SelfPeer.js
+++ src/Mentee/SelfPeer.js
@@ -1,18 +1,6 @@
 import React, { useState } from 'react';
 import './SelfPeer.css';
 
-const courses = [
-    { selectedphase: 'option1', selectedtopic: 'Phase 10' },
-    { selectedphase: 'option2', selectedtopic: 'Phase 20+70 OJT1' },
-    { selectedphase: 'option3', selectedtopic: 'Phase 20+70 OJT2' },
-    { selectedphase: 'option4', selectedtopic: 'Phase 20+70 OJT3' }
-];
-
-const roles = [
-    { selectedphase: 'option1', selectedrole: 'Self' },
-    { selectedphase: 'option2', selectedrole: 'Peer' }
-];
-
 const cards = [
     { title: 'SOLUTION Culture', selectedphase: 'option1', no: 'Batch ' },
     { title: '8 Behaviour Compentencies', selectedphase: 'option1', no: 'Batch ' },
@@ -57,10 +45,10 @@ const PeerQuestion = [
 ];
 
 const PeerList = [
-    { nama: 'Naufal Romiz', status: 'Done' },
-    { nama: 'Ali Alban', status: 'Not Yet' },
-    { nama: 'Emmanuela Evelyn', status: 'Not Yet' },
-    { nama: 'Doni', status: 'Done' }
+    { nama: 'Naufal Romiz', statusSolution: 'Done', statusBehaviour: 'Not Yet' },
+    { nama: 'Ali Alban', statusSolution: 'Not Yet', statusBehaviour: 'Not Yet' },
+    { nama: 'Emmanuela Evelyn', statusSolution: 'Not Yet', statusBehaviour: 'Not Yet' },
+    { nama: 'Doni', statusSolution: 'Done', statusBehaviour: 'Not Yet' }
 ];
 
 function SelfPeer() {
@@ -69,104 +57,120 @@ function SelfPeer() {
     const [selectedQuestion, setSelectedQuestion] = useState(null);
     const [peerList, setPeerList] = useState(PeerList);
     const [selectedPeer, setSelectedPeer] = useState(null);
-
-    const [selectedPhase, setSelectedPhase] = useState('option1');
-    const [selectedRole, setSelectedRole] = useState('Self');
-
-    const handlePhaseChange = (event) => {
-        setSelectedPhase(event.target.value);
-    };
-
-    const handleRoleChange = (event) => {
-        setSelectedRole(event.target.value);
-    };
+    const [answers, setAnswers] = useState({});
 
     const handleMain = () => {
         setCurrentPage('main');
         setSelectedTask(null);
         setSelectedQuestion(null);
         setSelectedPeer(null);
+        setAnswers({});
     };
 
-    const handleSecond = (task) => {
+    const handleSecond = (task, role) => {
         setSelectedTask(task);
-        if (selectedRole === 'Self' && task.title === 'SOLUTION Culture') {
+        if (role === 'Self') {
             setCurrentPage('second');
-            setSelectedQuestion('Solution');
-        } else if (selectedRole === 'Self' && task.title === '8 Behaviour Compentencies') {
-            setCurrentPage('second');
-            setSelectedQuestion('8 Behaviour Compentency');
-        } else if (selectedRole === 'Peer' && task.title === 'SOLUTION Culture') {
+            if (task.title === 'SOLUTION Culture') {
+                setSelectedQuestion('Solution');
+            } else if (task.title === '8 Behaviour Compentencies') {
+                setSelectedQuestion('8 Behaviour Compentency');
+            }
+        } else if (role === 'Peer') {
             setCurrentPage('peerList');
-            setSelectedQuestion('Solution');
-        } else if (selectedRole === 'Peer' && task.title === '8 Behaviour Compentencies') {
-            setCurrentPage('peerList');
-            setSelectedQuestion('8 Behaviour Compentency');
-        } else {
-            return null;
         }
     };
 
     const handlePeerSelected = (person) => {
-        if (person.status === 'Not Yet') {
+        if ((selectedTask.title === 'SOLUTION Culture' && person.statusSolution === 'Not Yet') ||
+            (selectedTask.title === '8 Behaviour Compentencies' && person.statusBehaviour === 'Not Yet')) {
             setSelectedPeer(person);
             setCurrentPage('second');
+            if (selectedTask.title === 'SOLUTION Culture') {
+                setSelectedQuestion('Solution');
+            } else if (selectedTask.title === '8 Behaviour Compentencies') {
+                setSelectedQuestion('8 Behaviour Compentency');
+            }
         }
     };
 
+    const handleAnswerClick = (questionIndex, value) => {
+        setAnswers(prevAnswers => ({
+            ...prevAnswers,
+            [questionIndex]: value
+        }));
+    };
+
+    const handleSubmit = () => {
+        const questions = selectedPeer ? 
+            PeerQuestion.filter(item => item.title2 === selectedQuestion) : 
+            SelfQuestion.filter(item => item.title2 === selectedQuestion);
+
+        for (let i = 0; i < questions.length; i++) {
+            if (!answers.hasOwnProperty(i)) {
+                alert("Ada pertanyaan yang belum diisi");
+                return;
+            }
+        }
+
+        if (selectedPeer) {
+            setPeerList(prevList => 
+                prevList.map(person => {
+                    if (person.nama === selectedPeer.nama) {
+                        if (selectedTask.title === 'SOLUTION Culture') {
+                            return { ...person, statusSolution: 'Done' };
+                        } else if (selectedTask.title === '8 Behaviour Compentencies') {
+                            return { ...person, statusBehaviour: 'Done' };
+                        }
+                    }
+                    return person;
+                })
+            );
+        }
+        handleMain();
+    };
+
     const renderCard = () => {
-        const filteredphase = cards.filter(card => card.selectedphase === selectedPhase);
+        const filteredphase = cards.filter(card => card.selectedphase === 'option1'); // Sesuaikan dengan logika pemilihan fase Anda
 
         return filteredphase.map((selfpeer, index) => (
             <div key={index} className="selfpeer-item">
                 <div className="description">
-                    <img className="selfpeer-img" src="/src/files/icons/TaskImg.png" alt="
-                    Task" />
+                    <img className="selfpeer-img" src="/src/files/icons/TaskImg.png" alt="Task" />
                     <div className="selfpeer-text">
-                        <div className="selfpeer-title">{selfpeer.title}</div>
+                        <div className="selfpeer-title"><b>{selfpeer.title}</b></div>
                         <div className="selfpeer-batch">{selfpeer.no}</div>
-                    </div>
-                </div>
+                    </div>                    
+                </div>     
                 <div className="TwinButton">
-                    <div className="selfpeer-button" onClick={() => handleSecond(selfpeer)}>Mulai Isi Penilaian</div>
-                </div>
+                        <button className="self-button" onClick={() => handleSecond(selfpeer, 'Self')}>Self</button>
+                        <button className="peer-button" onClick={() => handleSecond(selfpeer, 'Peer')}>Peer</button>
+                </div>           
             </div>
         ));
     };
 
     const renderQuestion = () => {
-        const questions1 = SelfQuestion.filter(item => item.title2 === selectedQuestion);
-        const questions2 = PeerQuestion.filter(item => item.title2 === selectedQuestion);
+        const questions = selectedPeer ? 
+            PeerQuestion.filter(item => item.title2 === selectedQuestion) : 
+            SelfQuestion.filter(item => item.title2 === selectedQuestion);
 
-        if (selectedRole === 'Self') {
-            return questions1.map((question, index) => (
-                <div key={index}>
-                    {index + 1}. {question.question}
-                    <div className="buttonPosition">
-                        <button className="btnGhost">1</button>
-                        <button className="btnGhost">2</button>
-                        <button className="btnGhost">3</button>
-                        <button className="btnGhost">4</button>
-                        <button className="btnGhost">5</button>
-                    </div>
+        return questions.map((question, index) => (
+            <div key={index}>
+                {index + 1}. {question.question}
+                <div className="buttonPosition">
+                    {[1, 2, 3, 4].map(value => (
+                        <button 
+                            key={value}
+                            className={`btnGhost ${answers[index] === value ? 'selected' : ''}`}
+                            onClick={() => handleAnswerClick(index, value)}
+                        >
+                            {value}
+                        </button>
+                    ))}
                 </div>
-            ));
-        } else if (selectedRole === 'Peer') {
-            return questions2.map((question, index) => (
-                <div key={index}>
-                    {index + 1}. {question.question}
-                    <div className="buttonPosition">
-                        <button className="btnGhost">1</button>
-                        <button className="btnGhost">2</button>
-                        <button className="btnGhost">3</button>
-                        <button className="btnGhost">4</button>
-                        <button className="btnGhost">5</button>
-                    </div>
-                </div>
-            ));
-        } else {
-            return null;
-        }
+            </div>
+        ));
     };
 
     const renderPage = () => {
@@ -177,27 +181,20 @@ function SelfPeer() {
                         <div className="title1">
                             <h><b>Assessment</b></h>
                         </div>
+                        
                         <div className="Dropdown">
-                            <div className="phasetitle">
+                             <div className="phasetitle">
                                 <a>Phase</a>
-                                <select className="phaseselect" value={selectedPhase} onChange={handlePhaseChange}>
+                                <select className="phaseselect">
                                     <option value="option1">Phase 10</option>
                                     <option value="option2">Phase 20+70 OJT1</option>
                                     <option value="option3">Phase 20+70 OJT2</option>
                                     <option value="option4">Phase 20+70 OJT3</option>
                                 </select>
                             </div>
-
-                            <div className="roletitle">
-                                <a>Role</a>
-                                <select className="role" value={selectedRole} onChange={handleRoleChange}>
-                                    <option value="Self">Self</option>
-                                    <option value="Peer">Peer</option>
-                                </select>
-                            </div>
                         </div>
                         <hr />
-                        <div className="selfpeer">
+                        <div className="selfpeer-rc">
                             {renderCard()}
                         </div>
                     </div>
@@ -206,23 +203,22 @@ function SelfPeer() {
                 return (
                     <div className="selfpeer">
                         <div className="title2">
-                            <img onClick={handleMain} src="/src/files/icons/backbutton.png" alt="Back Button" />
+                            <img className="backbutton" onClick={handleMain} src="/src/files/icons/backbutton.png" alt="Back Button" />
                             <h><b>{selectedQuestion}</b></h>
                         </div>
                         <hr />
                         <div className="question-container">
                             {renderQuestion()}
                             <div className="submitButton">
-                                <button className="submit"> <b>Submit</b> </button>
+                                <button className="submit" onClick={handleSubmit}> <b>Submit</b> </button>
                             </div>
                         </div>
                         <div className="question-container-scale">
                             <h><b>Skala Penilaian</b></h>
-                            <p className="numberOne">1 &nbsp;&nbsp; Sangat Kurang </p>
-                            <p className="numberTwo">2 &nbsp;&nbsp; Kurang </p>
-                            <p className="numberThree">3 &nbsp;&nbsp; Cukup </p>
-                            <p className="numberFour">4 &nbsp;&nbsp; Baik </p>
-                            <p className="numberFive">5 &nbsp;&nbsp; Sangat Baik </p>
+                            <p className="numberOne">1 &nbsp;&nbsp; Sangat Tidak Sesuai </p>
+                            <p className="numberTwo">2 &nbsp;&nbsp; Tidak Sesuai </p>
+                            <p className="numberThree">3 &nbsp;&nbsp; Sesuai </p>
+                            <p className="numberFour">4 &nbsp;&nbsp; Sangat Sesuai </p>
                         </div>
                     </div>
                 );
@@ -230,40 +226,44 @@ function SelfPeer() {
                 return (
                     <div className="selfpeer">
                         <div className="title3">
-                            <img src="/src/files/icons/backbutton.png" onClick={handleMain} alt="Back Button" />
+                            <img className="backbutton" src="/src/files/icons/backbutton.png" onClick={handleMain} alt="Back Button" />
                             <h><b>Peer Assessment - Daftar Nama</b></h>
                         </div>
                         <hr />
-                        <div className="table-header">
-                            <div className="peer-name">Nama</div>
-                            <div className="peer-status">Status</div>
-                        </div>
-                        <div className="peer-list">
-                            {peerList.map((person, index) => (
-                                <div
-                                    key={index}
-                                    className={`peer-item ${person.status === 'Done' ? 'disabled' : ''}`}
-                                    onClick={() => handlePeerSelected(person)}
-                                    style={{ cursor: person.status === 'Done' ? 'not-allowed' : 'pointer' }}
-                                >
-                                    <div className="peer-name">{person.nama}</div>
-                                    <div className="peer-status">{person.status}</div>
-                                </div>
-                            ))}
+                        <div className='table-PL'>
+                            <div className="table-header">
+                                <div className="peer-no">No</div>
+                                <div className="peer-name">Nama</div>
+                                <div className="peer-status">Status</div>
+                            </div>
+                            <div className="peer-list">
+                                {peerList.map((person, index) => (
+                                    <div
+                                        key={index}
+                                        className={`peer-item ${((selectedTask.title === 'SOLUTION Culture' && person.statusSolution === 'Done') ||
+                                            (selectedTask.title === '8 Behaviour Compentencies' && person.statusBehaviour === 'Done')) ? 'disabled' : ''}`}
+                                        onClick={() => handlePeerSelected(person)}
+                                        style={{ cursor: ((selectedTask.title === 'SOLUTION Culture' && person.statusSolution === 'Done') ||
+                                            (selectedTask.title === '8 Behaviour Compentencies' && person.statusBehaviour === 'Done')) ? 'not-allowed' : 'pointer' }}
+                                    >
+                                        <div className="peer-no">{index + 1}.</div>
+                                        <div className="peer-name">{person.nama}</div>
+                                        <div className="peer-status">
+                                            {selectedTask.title === 'SOLUTION Culture' ? person.statusSolution : person.statusBehaviour}
+                                        </div>
+                                    </div>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 );
+
             default:
                 return null;
         }
     };
 
-    return (
-        <div className="App">
-            {renderPage()}
-        </div>
-    );
+    return <div>{renderPage()}</div>;
 }
 
 export default SelfPeer;
-
